3112d68472c1e38c9cfbae091f08d4e6edb2bb79
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-11-16 15:08:53 +0900

    Title: 알파벳에 따른 평가, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231116/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+a = input()
+if a=='S':
+    print('Superior')
+elif a=='A':
+    print('Excellent')
+elif a=='B':
+    print('Good')
+elif a=='C':
+    print('Usually')
+elif a=='D':
+    print('Effort')
+else:
+    print('Failure ')
