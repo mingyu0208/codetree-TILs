9dcca2b97c880964c062913d94f1c0cf1297593b
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-11-21 23:19:43 +0900

    Title: 시력 검사, Time: 105ms, Memory: 25MB, Status: Passed - CodeTree

--- 231121/시력 검사/eye-test.py
+++ 231121/시력 검사/eye-test.py
@@ -1,9 +1,9 @@
 a = float(input())
 b = float(input())
 
-if a>=1.0 and b>=1.0:
+if a >= 1.0 and b >= 1.0:
     print('High')
-elif a>=5.0 and b>=5.0:
+elif a >=0.5 and b >= 0.5:
     print('Middle')
 else:
     print('Low')
