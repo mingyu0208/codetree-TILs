1f03b1d6c6ce8f9743065eb3367fb09c8d4231a5
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-11-21 23:25:40 +0900

    Title: 특정 조건 세 정수 비교, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231121/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,16 @@
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+if a <= b and a <= c:
+	print("1", end=" ")
+else:
+	print("0", end=" ")
+
+if a == b and b == c:
+	print("1")
+else:
+	print("0")
