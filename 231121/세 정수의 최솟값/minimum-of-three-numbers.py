b621accbc548c148258b56a6e07f190a2d30a98b
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-11-21 23:36:34 +0900

    Title: 세 정수의 최솟값, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231121/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 231121/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -3,9 +3,9 @@ a = int(inp[0])
 b = int(inp[1])
 c = int(inp[2])
 
-if a<b and a<c:
+if a<b and a<c or a>=b and a>=c:
 	print(a)
-elif b<a and b<c:
+elif b<a and b<c or b<=a and b<=c :
 	print(b)
-elif c<a and c<b:
+elif c<a and c<b or c<=a and c<=b:
 	print(c)
