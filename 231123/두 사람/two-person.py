b11a09b5825d425d870472dcc7caf082e93e7c27
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-11-23 21:06:36 +0900

    Title: 두 사람, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/두 사람/two-person.py
+++ 231123/두 사람/two-person.py
@@ -1,8 +1,8 @@
 inp = input().split()
-A_age, A_sex =int(inp[0]), str(inp[1])
-B_age,B_sex =int(inp[0]), str(inp[1])
+A_age, A_sex = int(inp[0]), str(inp[1])
+B_age,B_sex = int(inp[0]), str(inp[1])
 
-if (A_age>=19 and A_sex=='M') or (B_age>=19 and B_sex=='M'):
+if (A_age>=19 and A_sex=='M') or (B_age>=19 and B_sex=='M')or (A_age<19 and B_sex=='M'and B_age>=19)or (B_age<19 and A_age>=19 and A_sex=='M'):
     print(1)
 else:
     print(0)
