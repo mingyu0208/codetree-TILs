a471fa4158c105af63b3bc7cd1d03adea15b1178
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-12-05 21:22:07 +0900

    Title: 코로나 메뉴얼, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 231205/코로나 메뉴얼/covid-manual.py
+++ 231205/코로나 메뉴얼/covid-manual.py
@@ -1,9 +1,19 @@
-count=0
-for i in range(3):
-    a,b = input().split()
-    if a=="Y" and int(b)>=37:
-        count+=1
-if count>=2:
+a,b = input().split()
+c,d = input().split()
+e,f = input().split()
+Ac=0
+
+
+if (a=='Y' and int(b)>=37):
+    Ac+=1
+if (c=='Y' and int(d)>=37):
+    Ac+=1
+if (e=='Y' and int(f)>=37):
+    Ac+=1
+
+    
+
+if Ac>=2:
     print("E")
 else:
     print("N")
