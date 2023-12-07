a396be4bbad8be08925c6780b2c318c7e2643bc8
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2023-12-07 21:19:24 +0900

    Title: a/b 출력, Time: 335ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 231207/a/b 출력/a-divide-b.py
+++ 231207/a/b 출력/a-divide-b.py
@@ -3,4 +3,14 @@ a,b = map (int,input().split())
 one =a
 two = b
 
-print(f"{(decimal.Decimal(str(one))/two):.20f}")
+
+
+
+print(f"{one//two}.",end="")
+
+for i in range(20):
+    one*=10
+    print(one//two,end="")
+    one%=two
+
+# print(f"{(decimal.Decimal(str(one))/two):.20f}")
