46796e42bdfa1096b24bb41ac82186808ea23bb6
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-07 19:18:16 +0900

    Title: 출석 부르기 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240107/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True :
+    n = int(input())
+    if n==1:
+        print('John')
+    if n==2:
+        print('Tom')
+    if n==3:
+        print('Paul')
+    if n==4:
+        print('Sam')
+    if n>=5:
+        print('Vacancy')
+        break
