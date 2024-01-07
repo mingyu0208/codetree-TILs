13cfea28510d417ec1bbcc665486a43e833594b6
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-07 21:14:14 +0900

    Title: 몫 출력하기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/몫 출력하기/print-share.py
+++ 240107/몫 출력하기/print-share.py
@@ -1,9 +1,9 @@
 cnt = 0
 while True:
     n = int(input())
-    cnt+=1
     if n%2==0:
+        cnt+=1
         print(n//2)
-        if cnt<4:
+        if cnt<3:
             continue
         break
