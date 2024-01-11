f133d73b05258ef9c087d03f536fe77a2b614d36
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-11 14:46:15 +0900

    Title: 조건에 따른 연산, Time: 234ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/조건에 따른 연산/operatino-by-rule.py
+++ 240111/조건에 따른 연산/operatino-by-rule.py
@@ -4,10 +4,10 @@ cnt=0
 while True:
     if n>=1000:
         break
-    if n%2==0:
+    elif n%2==0:
         n = n*3+1
         cnt+=1
-    if n%2==1:
+    elif n%2==1:
         n = n*2+2
         cnt+=1
 
