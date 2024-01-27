c9aa2306173c06b9d7872286ee1c87a2cc42d973
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-27 23:56:44 +0900

    Title: 별표 출력하기 13, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240127/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,28 @@
+n = int(input())
+for i in range(1, n+1):
+    if i % 2 == 0:
+        print('* '*plus)
+        plus += 1
+    else:
+        print('* '*minus)
+        minus -= 1
+
+plus = (n//2 + 1)
+minus = n//2
+
+if n % 2 == 0:
+    for j in range(1, n +1):
+        if j % 2 == 0:
+            print('* '*plus )
+            plus += 1
+        else:
+            print('* '*minus)
+            minus-= 1
+else:
+    for k in range(1, n+1):
+        if k % 2 == 0:
+            print('* '*minus)
+            minus -= 1
+        else:
+            print('* '*plus)
+            plus += 1
