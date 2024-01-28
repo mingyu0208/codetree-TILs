bbd4af33d39febfc47900dd3111b859a4bb1412e
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-28 19:05:57 +0900

    Title: 별표 출력하기 13, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/별표 출력하기 13/print-start-13.py
+++ 240128/별표 출력하기 13/print-start-13.py
@@ -1,10 +1,10 @@
 n = int(input())
 
-for i in range(n):
-    if i%2==0:
-        for _ in range(n):
-            print("",end=" ")
-    else:
-        for _ in range(n):
-            print("*",end=" ")
-    print()
+for i in range(2 * n):
+	if i % 2 == 1:
+		for _ in range(1 + i // 2):
+			print("* ", end="")
+	else:
+		for _ in range(n - i // 2):
+			print("* ", end="")
+	print()
