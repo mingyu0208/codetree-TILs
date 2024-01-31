420b0a158d41246341abc76bdc1791b8f0c0d852
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-31 19:32:44 +0900

    Title: 특이한 출력, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/특이한 출력/special-print.py
+++ 240131/특이한 출력/special-print.py
@@ -2,7 +2,7 @@ n = int(input())
 
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if j+i==4:
+        if j+i==n:
             print(f'({i}, {j})',end="\n")
         else:
             print(f'({i}, {j})',end=" ")
