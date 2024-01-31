df31d551829ffd4554b3a2cf0244684d82fe0f9b
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-01-31 21:54:42 +0900

    Title: 알파벳 출력하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/알파벳 출력하기/print-alphabet.py
+++ 240131/알파벳 출력하기/print-alphabet.py
@@ -4,11 +4,8 @@ n = int(input())
 cnt = 'A'
 for i in range(n):
     for j in range(i+1):
-        if cnt!= '[':
-            print(cnt,end="")
-            cnt = chr(ord(cnt)+1)
-        else:
-            cnt='A'
-            print(cnt,end="")
-            cnt = chr(ord(cnt)+1)
+        print(cnt,end="")
+        cnt = chr(ord(cnt)+1)
+        if ord(cnt)> ord('Z'):
+            cnt = "A"      
     print()
