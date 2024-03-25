6c9e8dd2313bfc5c8f13bccd5c13cea35feccfc7
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-25 19:10:40 +0900

    Title: 999 또는 -999, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240325/999 또는 -999/999-or-999.py
@@ -0,0 +1,12 @@
+arr = list(map(int,input().split()))
+max_val = arr[0]
+min_val = arr[0]
+
+for i in arr:
+    if i ==999 or i == -999:
+        break
+    if i>max_val:
+        max_val = i
+    if i< max_val:
+        min_val = i
+print(max_val, min_val)
