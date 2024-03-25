fc6063e186680360488c27043a292915b538b642
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-25 17:01:03 +0900

    Title: 특정 위치의 문자, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240325/특정 위치의 문자/char-in-specific-location.py
+++ 240325/특정 위치의 문자/char-in-specific-location.py
@@ -1,5 +1,6 @@
 arr = [ 'L', 'E', 'B', 'R', 'O', 'S']
+a = input()
 
-for a,b in enumerate(arr):
-    if b =='L':
-        print(a)
+for i in range(0,5):
+    if arr[i] == a:
+        print(arr.index(a))
