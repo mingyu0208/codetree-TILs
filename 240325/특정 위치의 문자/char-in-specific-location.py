5048accd117206b972987d1fa43accf3b0865fea
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-25 17:05:19 +0900

    Title: 특정 위치의 문자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/특정 위치의 문자/char-in-specific-location.py
+++ 240325/특정 위치의 문자/char-in-specific-location.py
@@ -1,12 +1,17 @@
 arr = [ 'L', 'E', 'B', 'R', 'O', 'S']
 a = input()
-idx = -1
+# idx = -1
 
-for i in range(0,6):
-    if arr[i] == a:
-        idx = i
+# for i in range(0,6):
+#     if arr[i] == a:
+#         idx = i
 
-if idx == -1:
-    print("None")
+# if idx == -1:
+#     print("None")
+# else:
+#     print(idx)
+    
+if a not in arr:
+    pritn("None")
 else:
-    print(idx)
+    print(arr.index(a))
