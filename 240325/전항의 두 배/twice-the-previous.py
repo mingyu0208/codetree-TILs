9fa9a625150c9911ad407e775138770e733fb4d5
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-25 21:08:26 +0900

    Title: 전항의 두 배, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/전항의 두 배/twice-the-previous.py
+++ 240325/전항의 두 배/twice-the-previous.py
@@ -1,8 +1,8 @@
-arr = list(map(int, input().split()))
-i = 0
+a,b = list(map(int, input().split()))
 
-while len(arr) != 12: # 이 부분에서 배열의 길이가 왜 12여야 정답이 되는 건가요?
-    x = (arr[-2] * 2) + arr[-1]
-    arr.append(x)
-    print(arr[i], end=" ")
-    i += 1
+arr = [a,b]
+for i in range(2,10):
+    arr.append(2 * arr[i-2] + arr[i-1])
+
+for elem in arr:
+    print(elem,end= " ")
