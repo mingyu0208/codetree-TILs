232ffd7c1aacb97ad1bf55801e9cdfa336cc48b9
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-27 02:03:47 +0900

    Title: 나눗셈의 나머지, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/나눗셈의 나머지/remainder-of-division.py
+++ 240327/나눗셈의 나머지/remainder-of-division.py
@@ -1,8 +1,14 @@
 a,b = map(int,input().split())
-cnt = [0] * 10
-while a > 1:
-    cnt[a%b] += 1
-    a = a // b
+count_arr = [0] * 10
+ans = 0
 
-cnt = [elem ** 2 for elem in cnt]
-print(sum(cnt))
+while a > 1:
+    count_arr[a % b] += 1
+    a //= b
+	
+
+for elem in count_arr:
+    ans += elem ** 2
+
+
+print(ans)
