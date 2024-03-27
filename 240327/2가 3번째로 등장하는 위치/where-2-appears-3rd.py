d7d5ca93934b9d074082aa307cea2941d0f60bb9
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-27 18:52:56 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240327/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,11 +1,13 @@
 n = int(input())
 arr = list(map(int,input().split()))
-cnt = 0
-for a,b in enumerate(arr):
-    while cnt<3:
-        if b == 2:
-            cnt+=1
-    
-if cnt==3:
-    # print(arr[a])
-    print(arr[b]+1)
+cnt = arr.count(2)
+cnt_2 = 0
+
+for i in range(n):
+    if cnt_2 <3:
+        if arr[i] != 2:
+            cnt-=1
+            cnt_2+=1
+    else:
+        break
+print(cnt+1)
