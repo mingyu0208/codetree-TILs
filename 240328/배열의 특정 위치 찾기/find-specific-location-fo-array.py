777b5974598e59715453e67b5dedc2f932847740
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-28 21:09:31 +0900

    Title: 배열의 특정 위치 찾기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240328/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split()))
+
+n = len(arr)
+
+sum2 = 0
+sum3 = 0
+cnt = 0
+
+for i in range(n):
+    if (i+1)%2==0:
+        sum2+= arr[i]
+    if (i+1)%3==0:
+        sum3+=arr[i]
+        cnt+=1
+avg3 = sum3/cnt
+
+print(f'{sum2} {avg3:.1f}')
