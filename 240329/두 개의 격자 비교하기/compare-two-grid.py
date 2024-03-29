14f48ea8120204a840cd4e5f4e9f27165c4a6e07
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-03-29 17:02:02 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240329/두 개의 격자 비교하기/compare-two-grid.py
+++ 240329/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,6 +1,6 @@
 n,m = map(int,input().split())
 
-arr_1d = [list(map(int,input().split())) for _ in range(m)]
+arr_1d = [list(map(int,input().split())) for _ in range(n)]
 arr_2d = [list(map(int,input().split())) for _ in range(m)]
 for i in range(n):
     for j in range(m):
