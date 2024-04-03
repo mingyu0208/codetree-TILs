adb31fe3ae4ea045326bc12f0a456c0b5086ca3a
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-04-04 01:11:31 +0900

    Title: 격자로 사각형 만들기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240404/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+arr_2d = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(n):
+    arr_2d[0][i] = 1
+    arr_2d[i][0] = 1
+
+
+for i in range(1,n):
+    for j in range(1,n):
+        arr_2d[i][j] = arr_2d[i][j-1] + arr_2d[i-1][j] + arr_2d[i-1][j-1]
+
+for i in arr_2d:
+    print(*i)
