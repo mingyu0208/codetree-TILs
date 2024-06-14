f746d3b9a0f8df1332e54835377099851071fd9b
kimmingyu <150799664+mingyu0208@users.noreply.github.com>
2024-06-15 01:54:46 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240615/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.py
+++ 240615/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.py
@@ -1,7 +1,13 @@
 a =input()
 b = input()
+ans = 'false'
+aPb = a+b
+bPa = b+a
 
-if a in b:
-    print('true')
-else:
-    print('false')
+if aPb == bPa:
+    ans = 'true'
+print(ans)
+# for i in aPb:
+#     if i == bPa:
+#         ans = 'true'
+# print(ans)
